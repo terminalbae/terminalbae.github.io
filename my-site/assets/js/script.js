b8f2dea086b372a41bc16c34ef403c3a6b21c454
Andrew Nichols <aln2129@gmail.com>
2024-10-19 00:55:05 -0300

    unnest folder

--- my-site/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.nav-link').on('click', function(e) {
-        e.preventDefault(); // Prevent the default link behavior
-        var page = $(this).data('page'); // Get the data-page attribute
-
-        // Load the content into the main area
-        $('#content').load(page + ' #content > *'); // Load content only from the #content section of the page
-    });
-});
